fa8680fafa5006c02dc9b0084373d3f9a46cf800
Matt Engledowl <mengledowl@gmail.com>
2017-01-17 15:54:26 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function greet() {
+  console.log("hi");
+}
+
+greet();
+
+function logGreeting(fn) {
+  fn();
+}
+
+logGreeting(greet);
+
+var greetMe = function() {
+  console.log('Hi Tony');
+}
+greetMe();
+
+logGreeting(greetMe);
+
+logGreeting(function() {
+  console.log('Hello tony!');
+})
