561ac67a5ff7506462ba21721a4dff67628ddfa6
Matt Engledowl <mengledowl@gmail.com>
2017-01-18 13:57:07 -0600

    Buffers, callbacks, syntactic sugar for classes, typed arrays

--- emitter_inheritance/app.js
+++ emitter_inheritance/app.js
@@ -1,16 +1,6 @@
-var EventEmitter = require('events');
-var util = require('util');
+'use strict';
 
-function Greetr() {
-  this.greeting = 'Hello world!';
-}
-
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greet = function (data) {
-  console.log(this.greeting + ': ' + data);
-  this.emit('greet', data);
-};
+var Greetr = require('./greetr');
 
 var greeter1 = new Greetr();
 
