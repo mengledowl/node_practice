ce16e5ee69fed729ab57684a63f78d7395f4fa71
Matt Engledowl <mengledowl@gmail.com>
2017-01-19 12:13:17 -0600

    Use mongoose

--- express/app.js
+++ express/app.js
@@ -3,6 +3,44 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 
+var mongoose = require('mongoose');
+
+mongoose.connect('[addr]');
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+  firstname: String,
+  lastname: String,
+  address: String
+});
+
+var Person = mongoose.model('Person', personSchema);
+
+var john = Person({
+  firstname: 'John',
+  lastname: 'Doe',
+  address: '555 Main St.'
+});
+
+john.save(function(err) {
+  if(err) throw err;
+
+  console.log('person saved!');
+});
+
+var jane = Person({
+  firstname: 'Jane',
+  lastname: 'Doe',
+  address: '555 Main St.'
+});
+
+jane.save(function(err) {
+  if(err) throw err;
+
+  console.log('person saved!');
+});
+
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
 
@@ -19,6 +57,14 @@ app.set('view engine', 'ejs');
 
 app.use('/', function(req, res, next) {
   console.log('Request Url: ' + req.url);
+
+  // get all users
+  Person.find({}, function (err, users) {
+    if(err) throw err;
+
+    console.log(users);
+  });
+
   next();
 });
 
